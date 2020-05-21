a264c77fddc97b768d02bed17e07622beac4e0aa
Diego <recsvintage@gmail.com>
2020-05-21 01:01:00 -0300

    runtime error in styles

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
+/*
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
+*/
